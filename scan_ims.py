7384974d11c31adbd6526751417c02fb222f5050
malleyconnor <malleyconnor@knights.ucf.edu>
2021-11-12 16:45:35 -0500

    Separating database based on resolution

--- scan_ims.py
+++ scan_ims.py
@@ -3,30 +3,46 @@ import os
 import numpy as np
 import magic
 import re
+import shutil
+from time import sleep
+from tqdm import tqdm
 
 res_set = {}
 
+res_to_get = ('640', '480')
 train_path = "/mnt/a/data/256_object/256_ObjectCategories/"
 train_dirs = os.listdir(train_path)
 im_paths = []
 
-for d in train_dirs:
-    this_dir = os.listdir(train_path+d)
+print("Scanning image directory contents...")
+for i in tqdm(range(len(train_dirs))):
+    this_dir = os.listdir(train_path+train_dirs[i])
     for im in this_dir:
-        im_paths.append(train_path + d + '/' + im)
+        im_paths.append(train_path + train_dirs[i] + '/' + im)
     
 
 res_counts = {}
-for im in im_paths:
+print("Relocating %s res images.."%(str(res_to_get)))
+for i in tqdm(range(len(im_paths))):
+    im = im_paths[i]
     image = magic.from_file(im)
-    im_shape = re.search('(\d+)x(\d+)', image)
-    if im_shape == None:
+    im_shape = re.findall('(\d+)x(\d+)', image)
+    if len(im_shape) < 2:
         continue
-    im_shape = im_shape.groups()
+    im_shape = re.findall('(\d+)x(\d+)', image)[1]
     if im_shape not in res_counts.keys():
         res_counts[im_shape] = 0
 
     res_counts[im_shape] += 1
+    if im_shape == res_to_get:
+        try:
+            os.mkdir('/mnt/a/data/256_object/640_480')
+        except (OSError):
+            ...
+
+        shutil.move(im, '/mnt/a/data/256_object/640_480/' + im.split('/')[-1])
+    
+
 
 
 maxcount = 0
